ce0578a254a855693359b7457754872e50cf69c2
adrian-naziru <adrian-marian.naziru@student.tuiasi.ro>
2025-03-05 19:40:16 +0200

    Add files via upload

---
+++ Laborator_3/include/version.h
@@ -0,0 +1,10 @@
+
+#ifndef BUILD_NUMBER
+  #define BUILD_NUMBER "4"
+#endif
+#ifndef VERSION
+  #define VERSION "v0.1.4 - 2025-03-05 19:33:04.128240"
+#endif
+#ifndef VERSION_SHORT
+  #define VERSION_SHORT "v0.1.4"
+#endif
